b0e6ab568c06e2304a32daacef60aa93fdc137e0
GameTo <1374434199@qq.com>
2018-09-20 16:02:29 +0800

    路由与视图

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,13 @@
+<div class="col-md-12">
+    <footer class="footer">
+        <small class="slogon">
+            <img class="brand-icon" src="https://lccdn.phphub.org/uploads/images/201612/12/1/iq7WQc2iuW.png?imageView2/1/w/34/h/34" >
+            <a href="http://wangzhenyu.tk">直面恐惧，战胜恐惧</a>
+        </small>
+        <nav>
+            <ul>
+                <li><a href="{{ route('about') }}">关于作者</a></li>
+            </ul>
+        </nav>
+    </footer>
+</div>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,5 +1,12 @@
 @extends('layouts.default')
 @section('title','主页')
 @section('content')
-    <h1>主页</h1>
+    <div class="jumbotron">
+        <h1>这是我的第一个laravel程序</h1>
+        <p class="lead">请前往我的<a href="http://wangzhenyu.tk">个人站观摩</a></p>
+        <p>万里长征第一步！</p>
+        <p>
+            <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">现在注册</a>
+        </p>
+    </div>
 @stop
